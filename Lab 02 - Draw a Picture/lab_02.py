7beb017e1a4a656dd90c764330797e62e72f4b2f
Jerrell Carter <jerrell.carter@my.simpson.edu>
2021-09-16 15:45:48 -0500

    lab 03

--- Lab 02 - Draw a Picture/lab_02.py
+++ Lab 02 - Draw a Picture/lab_02.py
@@ -18,21 +18,50 @@ arcade.start_render()
 
 # Draw sun set
 arcade.draw_lrtb_rectangle_filled(0, 600, 550, 450, arcade.color.ORANGE_PEEL)
-arcade.draw_lrtb_rectangle_filled(0, 600, 450, 400, arcade.color.DEEP_SAFFRON)
-arcade.draw_lrtb_rectangle_filled(0, 600, 400, 350, arcade.color.DEEP_CARROT_ORANGE)
-arcade.draw_lrtb_rectangle_filled(0, 600, 350, 300, arcade.color.ORIOLES_ORANGE)
-arcade.draw_lrtb_rectangle_filled(0, 600, 300, 150, arcade.color.ORANGE_RED)
+arcade.draw_lrtb_rectangle_filled(0, 600, 450, 400, arcade.color.DARK_ORANGE)
+arcade.draw_lrtb_rectangle_filled(0, 600, 400, 340, arcade.color.PUMPKIN)
+arcade.draw_lrtb_rectangle_filled(0, 600, 340, 150, arcade.color.ORANGE_RED)
 
 
-# Draw the grass
+# Draw the grass with STL design
 arcade.draw_lrtb_rectangle_filled(0, 600, 250, 0, arcade.color.APPLE_GREEN)
+arcade.draw_text("S T L",
+                 150, 100,
+                 arcade.color.GREEN_YELLOW, 100)
 
 # Draw the arch
 arcade.draw_arc_filled(300, 250, 400, 600, arcade.color.DARK_GRAY, 0, 180)
-arcade.draw_arc_filled(300, 250, 350, 550, arcade.color.SAE, 0, 180)
+arcade.draw_arc_filled(300, 250, 350, 550, arcade.color.ORANGE_PEEL, 0, 180)
+arcade.draw_arc_filled(300, 250, 350, 450, arcade.color.DARK_ORANGE, 0, 180)
+arcade.draw_arc_filled(300, 250, 350, 400, arcade.color.PUMPKIN, 0, 180)
+arcade.draw_arc_filled(300, 250, 350, 350, arcade.color.ORANGE_RED, 0, 180)
+
+# Draw buildings with windows
+arcade.draw_lrtb_rectangle_filled(0, 50, 400, 250, arcade.color.BLACK)
+arcade.draw_lrtb_rectangle_filled(10, 20, 390, 380, arcade.color.WHITE)
+arcade.draw_lrtb_rectangle_filled(10, 20, 370, 360, arcade.color.WHITE)
+arcade.draw_lrtb_rectangle_filled(10, 20, 350, 340, arcade.color.WHITE)
+arcade.draw_lrtb_rectangle_filled(30, 40, 390, 380, arcade.color.WHITE)
+arcade.draw_lrtb_rectangle_filled(30, 40, 370, 360, arcade.color.WHITE)
+arcade.draw_lrtb_rectangle_filled(30, 40, 350, 340, arcade.color.WHITE)
+arcade.draw_lrtb_rectangle_filled(50, 80, 350, 250, arcade.color.BLACK)
+arcade.draw_lrtb_rectangle_filled(60, 70, 340, 330, arcade.color.WHITE)
+arcade.draw_lrtb_rectangle_filled(60, 70, 320, 310, arcade.color.WHITE)
+arcade.draw_lrtb_rectangle_filled(550, 600, 400, 250, arcade.color.BLACK)
+arcade.draw_lrtb_rectangle_filled(580, 590, 390, 380, arcade.color.WHITE)
+arcade.draw_lrtb_rectangle_filled(580, 590, 370, 360, arcade.color.WHITE)
+arcade.draw_lrtb_rectangle_filled(580, 590, 350, 340, arcade.color.WHITE)
+arcade.draw_lrtb_rectangle_filled(560, 570, 390, 380, arcade.color.WHITE)
+arcade.draw_lrtb_rectangle_filled(560, 570, 370, 360, arcade.color.WHITE)
+arcade.draw_lrtb_rectangle_filled(560, 570, 350, 340, arcade.color.WHITE)
+arcade.draw_lrtb_rectangle_filled(520, 550, 350, 250, arcade.color.BLACK)
+arcade.draw_lrtb_rectangle_filled(530, 540, 340, 330, arcade.color.WHITE)
+arcade.draw_lrtb_rectangle_filled(530, 540, 320, 310, arcade.color.WHITE)
+
 
 # Draw the sun
-arcade.draw_arc_filled(300, 250, 200, 200, arcade.color.FLUORESCENT_ORANGE, 0, 180)
+arcade.draw_arc_filled(300, 250, 200, 200, arcade.color.SAE, 0, 180)
+
 
 # Finish Drawing
 arcade.finish_render()
---
+++ Testing/drawing_test 2.py
@@ -0,0 +1,15 @@
+def print_hello():
+    """ This function prints hello. """
+    print("Hello!")
+
+
+def goodbye():
+    print("Goodbye!")
+
+
+def main():
+    print_hello()
+    goodbye()
+
+
+main()
