9ef73022e71bb889c229e24253ebd41e90e6384f
Jerrell Carter <jerrell.carter@my.simpson.edu>
2021-11-03 11:48:43 -0500

    Lab 08

--- Lab 08 - Sprites/lab_08.py
+++ Lab 08 - Sprites/lab_08.py
@@ -5,15 +5,16 @@ import arcade
 
 # --- Constants ---
 SPRITE_SCALING_PLAYER = 0.2
-SPRITE_SCALING_CLARANET = 0.2
+SPRITE_SCALING_CLARINET = 0.2
 SPRITE_SCALING_KRABBY_PATTY = 0.2
-CLARANET_COUNT = 50
-KRABBY_PATTY_COUNT = 60
+CLARINET_COUNT = 50
+KRABBY_PATTY_COUNT = 25
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
-class KRABBYPATTY(arcade.Sprite):
+
+class Krabbypatty(arcade.Sprite):
     def __init__(self, filename, sprite_scaling):
         super().__init__(filename, sprite_scaling)
 
@@ -31,6 +32,18 @@ class KRABBYPATTY(arcade.Sprite):
         self.circle_angle += self.circle_speed
 
 
+class Clarinet(arcade.Sprite):
+    def reset_pos(self):
+        self.center_y = random.randrange(SCREEN_HEIGHT + 20,
+                                         SCREEN_HEIGHT + 100)
+        self.center_x = random.randrange(SCREEN_WIDTH)
+
+    def update(self):
+        self.center_y -= 1
+        if self.top < 0:
+            self.reset_pos()
+
+
 
 class MyGame(arcade.Window):
     """ Our custom Window Class"""
@@ -41,7 +54,7 @@ class MyGame(arcade.Window):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Sprite Example")
 
         self.player_list = None
-        self.claranet_list = None
+        self.clarinet_list = None
         self.krabby_patty_list = None
 
         self.player_sprite = None
@@ -53,7 +66,7 @@ class MyGame(arcade.Window):
 
     def setup(self):
         self.player_list = arcade.SpriteList()
-        self.claranet_list = arcade.SpriteList()
+        self.clarinet_list = arcade.SpriteList()
         self.krabby_patty_list = arcade.SpriteList()
         self.score = 0
         # Squidward is from Elan Arad for WhatsApp
@@ -62,17 +75,18 @@ class MyGame(arcade.Window):
         self.player_sprite.center_y = 50
         self.player_list.append(self.player_sprite)
 
-        for i in range(CLARANET_COUNT):
-            # Claranet is from The Parody Wiki
-            claranet = arcade.Sprite("Squidward%27s_Clarinet.png", SPRITE_SCALING_CLARANET)
+        for i in range(CLARINET_COUNT):
+            # Clarinet is from The Parody Wiki
+            clarinet = Clarinet("Squidward%27s_Clarinet.png", SPRITE_SCALING_CLARINET)
 
-            claranet.center_x = random.randrange(SCREEN_WIDTH)
-            claranet.center_y = random.randrange(SCREEN_HEIGHT)
-            self.claranet_list.append(claranet)
+
+            clarinet.center_x = random.randrange(SCREEN_WIDTH)
+            clarinet.center_y = random.randrange(SCREEN_HEIGHT)
+            self.clarinet_list.append(clarinet)
 
         for i in range(KRABBY_PATTY_COUNT):
 
-            krabby_patty = KRABBYPATTY("Krabby_Patty_transparentpng.png", SPRITE_SCALING_KRABBY_PATTY)
+            krabby_patty = Krabbypatty("Krabby_Patty_transparentpng.png", SPRITE_SCALING_KRABBY_PATTY)
             krabby_patty.circle_center_x = random.randrange(SCREEN_WIDTH)
             krabby_patty.circle_center_y = random.randrange(SCREEN_HEIGHT)
             krabby_patty.circle_radius = random.randrange(12, 200)
@@ -85,7 +99,7 @@ class MyGame(arcade.Window):
     def on_draw(self):
         arcade.start_render()
         self.player_list.draw()
-        self.claranet_list.draw()
+        self.clarinet_list.draw()
         self.krabby_patty_list.draw()
         output = f"Score: {self.score}"
         arcade.draw_text(output, 10, 20, arcade.color.WHITE, 14)
@@ -95,13 +109,14 @@ class MyGame(arcade.Window):
         self.player_sprite.center_y = y
 
     def update(self, delta_time):
-        self.claranet_list.update()
         self.krabby_patty_list.update()
-        good_hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.claranet_list)
+        self.clarinet_list.update()
+        good_hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.clarinet_list)
         bad_hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.krabby_patty_list)
 
-        for claranet in good_hit_list:
-            claranet.remove_from_sprite_lists()
+        for clarinet in good_hit_list:
+            clarinet.remove_from_sprite_lists()
+            clarinet.reset_pos()
             self.score += 1
 
         for krabby_patty in bad_hit_list:
---
+++ Testing/searching.py
@@ -0,0 +1,15 @@
+def main():
+    my_file = open("super_villains.txt")
+
+    my_list = []
+
+    for line in my_file:
+        line = line.strip()
+        print(line)
+        my_list.append()
+
+    my_file.close()
+
+
+
+main()
